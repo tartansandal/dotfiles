2d83e4eeca5e9233e23c4d71fb0996084edeac4b
Kahlil Hodgson <unknown@email.com>
2020-01-14 15:49:34 +1100

    Local setting for markdownlinter

---
+++ mdl-style.rb
@@ -0,0 +1,29 @@
+# include all rules by default
+all
+
+# include a specific rule:
+#
+#   rule 'MD001'
+
+# exclude_rule - exclude a previously included rule:
+#
+#   exclude_rule 'MD000'
+
+# include all rules that are tagged with a specific value:
+#
+#   tag :whitespace
+
+# exclude all rules tagged with the specified tag:
+#
+#   exclude_tag :line_length
+
+# include a rule but set options:
+#
+#   rule 'MD030', :ol_multi => 2, :ul_multi => 3
+
+# GitHub Pages
+exclude_rule 'first-header-h1'
+exclude_rule 'first-line-h1'
+rule 'ul-indent', :indent => 2
+rule 'ol-prefix', :style => 'ordered'
+exclude_rule 'code-block-style'
