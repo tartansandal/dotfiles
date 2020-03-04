3fb8eeb9597c08d1bae78e007a7f55840bd838cc
Kahlil Hodgson <unknown@email.com>
2020-03-04 16:56:26 +1100

    Make reset fonts work with both X11 and Wayland

--- bin/reset-fonts.sh
+++ bin/reset-fonts.sh
@@ -5,7 +5,7 @@ gsettings set org.gnome.desktop.interface font-name 'Cantarell 11'
 gsettings set org.gnome.desktop.interface document-font-name 'Sans 11'
 gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Cantarell Bold 11'
 
-if xrandr -q | grep -q 'connected 3840x2160'
+if xrandr -q | grep -Eq 'connected.*3840x2160'
 then
     gsettings set org.gnome.desktop.interface text-scaling-factor 0.75
     message='Re-setting fonts for\n"connected monitor"'
