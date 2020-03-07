d5c46372c0a0d5fe20cfd4e712223cdd7e9d1736
Kahlil Hodgson <unknown@email.com>
2020-03-07 12:31:20 +1100

    Update module resigning for nvidia
    Also add a trap to ensure we dont accidentally sign something we dont
    want to.

--- resign-modules.sh
+++ resign-modules.sh
@@ -4,14 +4,39 @@ set -e
 
 sudo --validate
 
+# only look for modules matching this find spec
+matchers="-name 'vbox*.ko' -o -name nvidia*.ko"
+
 # If we have just installed a kernel, it may not be the current, so we just
-# sign everything we can find
+# try to sign everything we can find
 for kernel in $(ls -1 /usr/src/kernels/ | grep -v debug)
 do
-    modules=$(find /lib/modules/$kernel -name 'vbox*.ko' -o -name 'evdi.ko' 2>/dev/null)
+    modules=$(find /lib/modules/$kernel $matchers 2>/dev/null)
+
+    if [[ -z $modules ]]
+    then
+        echo "Could not find any modules to sign for kernal $kernel"
+        continue
+    fi
+
+    echo "Found the following modules for kernel $kernel"
     for module in ${modules[*]}
     do
-        echo signing $module
-        sudo /usr/src/kernels/$kernel/scripts/sign-file sha256 MOK.priv MOK.der $module
+        echo $module
     done
+
+    read -p "Do you want to sign these modules? (y|N) " reply
+
+    if [[ $reply == 'y' || $reply == 'Y' ]]
+    then
+        for module in ${modules[*]}
+        do
+            echo signing $module
+            sudo /usr/src/kernels/$kernel/scripts/sign-file sha256 MOK.priv MOK.der $module
+        done
+    fi
 done
+
+# Try to load the matching drivers
+sudo modprobe vboxdrv
+sudo modprobe nvidia
