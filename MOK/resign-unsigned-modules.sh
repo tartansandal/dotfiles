92abd846f12dbe429463a59468dc567d18f6cb1e
Kahlil Hodgson <unknown@email.com>
2020-06-15 12:56:34 +1000

    Improve logic in module resigning script

--- MOK/resign-unsigned-modules.sh
+++ MOK/resign-unsigned-modules.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 
-
-modules=$(find /lib/modules -name '*.ko' -exec grep -FL '~Module signature appended~' {} \+)
-
 set -e
-
-if [[ -z $modules ]]
-then
-    echo "Could not find any unsigned modules"
-    echo
-    exit
-fi
 
 sudo --validate
 
-for module in ${modules[*]}
+for kernel_path in /usr/src/kernels/*
 do
-    echo "Found the following unsinged module:"
-    echo $module
-    read -p "Do you want to sign this module? (y|N) " reply
+    kernel=$(basename $kernel_path)
+    modules=$(find /lib/modules/$kernel -name '*.ko' \
+                -exec grep -FL '~Module signature appended~' {} \+ )
+
+    if [[ -z $modules ]]
+    then
+        echo "Could not find any unsigned modules for kernel-$kernel"
+        continue
+    fi
+
+    printf "\nFound the following unsigned modules for kernel-$kernel:\n\n"
+    for module_path in ${modules[*]}
+    do
+        printf "\t$(basename $module_path .ko)\n"
+    done
+    echo
+
+    read -p "Do you want to sign these modules? (y|N) " reply
 
     if [[ $reply == 'y' || $reply == 'Y' ]]
     then
-        for module in ${modules[*]}
-        do
-            [[ $foo =~ /lib/modules/([^/]*)/ ]]
-            kernel=${BASH_REMATCH[1]}
-            echo signing $(basename $module)
-            sudo /usr/src/kernels/$kernel/scripts/sign-file sha256 MOK.priv MOK.der $module
-        done
         echo
+        for module_path in ${modules[*]}
+        do
+            echo signing $module_path
+            sudo /usr/src/kernels/$kernel/scripts/sign-file \
+                sha256 MOK.priv MOK.der $module_path
+        done
     fi
-    done
-echo
+    echo
+done
 
 # Try to load the matching drivers
 # sudo modprobe vboxdrv
