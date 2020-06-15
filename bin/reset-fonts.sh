3146f4d65317391431a1b7d67c6d3d7f9ac32a09
Kahlil Hodgson <unknown@email.com>
2020-06-15 12:57:51 +1000

    Improve logic in font reseting script

--- bin/reset-fonts.sh
+++ bin/reset-fonts.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 gsettings set org.gnome.desktop.interface monospace-font-name 'DejaVu Sans Mono Book 13'
-gsettings set org.gnome.desktop.interface font-name 'Cantarell 11'
-gsettings set org.gnome.desktop.interface document-font-name 'Sans 11'
-gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Cantarell Bold 11'
+gsettings set org.gnome.desktop.interface font-name 'Cantarell 10'
+gsettings set org.gnome.desktop.interface document-font-name 'Sans 10'
+gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Cantarell Bold 10'
 
-if xrandr -q | grep -Eq 'connected.*3840x2160'
+if [[ $(xrandr -q | grep -c 'connected') -gt 1 ]]
 then
     gsettings set org.gnome.desktop.interface text-scaling-factor 0.75
-    message='Re-setting fonts for\n"connected monitor"'
+    message='Re-setting fonts for "connected monitor"'
 else
     gsettings set org.gnome.desktop.interface text-scaling-factor 1.0
-    message='Re-setting fonts for\n"laptop only"'
+    message='Re-setting fonts for "laptop only"'
 fi
 
-zenity --info --text "$message" --no-wrap --height 100
+zenity --info --text "$message" --no-wrap 
