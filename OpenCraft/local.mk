33504da7ac3c534a137264d686c6fe854dc47597
Kahlil Hodgson <unknown@email.com>
2021-02-28 10:11:33 +1100

    Update makefile to use canned recipe and call

--- OpenCraft/local.mk
+++ OpenCraft/local.mk
@@ -15,36 +15,34 @@ my.clean-root-pyc-files:
 my.find-root-pyc-files:
 	find .. -name "*.pyc" -user 'root'
 
+# Do git action on all repos in my devstack workspace
+define git-all
+	@echo -e "\n>>> $1 out all repos...\n"
+	@for d in $$(ls ..); do          \
+		if test -d ../$$d/.git; then \
+			echo -e "\n$1 $$d";      \
+			git -C ../$$d $1 .;      \
+		else                         \
+			true;                    \
+		fi;                          \
+	done
+	@echo -e "\n<<< $1 out all repos...done\n"
+endef
+
 # Sometimes build leaves weird workfiles lying around which block pulling the
 # fresh code. Run `checkout` at top level to get us back to a sane state.
 .PHONY: my.checkout-all
 my.checkout-all: my.clean-root-pyc-files
-	# Checking out all repos...
-	@for d in $$(ls ..); do                  \
-		if test -d ../$$d/.git; then         \
-			echo -e "\nChecking out $$d";    \
-			git -C ../$$d checkout .;        \
-		else                                 \
-			true;                            \
-		fi;                                  \
-	done
-	# Checking out all repos...done
+	$(call git-all,checkout)
 
-# Make sure we are running the most up2date code
+# Make sure we are running the most up to date code
+.PHONY: my.update-all
 my.update-all: my.checkout-all
-	# Updating all repos...
-	@for d in $$(ls ..); do                  \
-		if test -d ../$$d/.git; then         \
-			echo -e "\nChecking out $$d";    \
-			git -C ../$$d pull .;            \
-		else                                 \
-			true;                            \
-		fi;                                  \
-	done
-	# Updating all repos...done
+	$(call git-all,pull)
 
-# For LabXchange development
+# For LabXchange development.
 # Ensure only lms and studio are running so we don't run out of memory
+# and ensure newest versions are being run
 .PHONY: my.labxchange
 my.labxchange:               \
 	requirements             \
