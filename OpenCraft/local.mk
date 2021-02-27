b089e3852cef0c18c9ab2d83b9333751ef3cbac0
Kahlil Hodgson <unknown@email.com>
2021-02-27 17:40:46 +1100

    Improve LX routines

--- OpenCraft/local.mk
+++ OpenCraft/local.mk
@@ -5,21 +5,54 @@ my.lms-watch: ## Rebuild static assets for the LMS container
 
 # Could suppress this by pushing PYTHONDONTWRITEBYTECODE=1 to environments that 
 # run the code as root, but that needs to be pushed into the docker containers.
-.PHONY: my.clean-pyc
-my.clean-pyc: ## Remove all pyc files (often created as root)
+.PHONY: my.clean-root-pyc-files
+my.clean-root-pyc-files: 
+	# Removing pyc files owned by root
 	sudo find .. -name "*.pyc" -user 'root' -delete
 
-.PHONY: my.find-root-pyc
-my.pyc-find-root:
+# for testing
+.PHONY: my.find-root-pyc-files
+my.find-root-pyc-files:
 	find .. -name "*.pyc" -user 'root'
 
 # Sometimes build leaves weird workfiles lying around which block pulling the
 # fresh code. Run `checkout` at top level to get us back to a sane state.
 .PHONY: my.checkout-all
-my.checkout-all:
-	for d in $$(ls ..); do git -C ../$$d checkout .; done
+my.checkout-all: my.clean-root-pyc-files
+	# Checking out all repos...
+	@for d in $$(ls ..); do                  \
+		if test -d ../$$d/.git; then         \
+			echo -e "\nChecking out $$d";    \
+			git -C ../$$d checkout .;        \
+		else                                 \
+			true;                            \
+		fi;                                  \
+	done
+	# Checking out all repos...done
 
-# For LabXchange development, ensure only lms and studio are running so we don't
-# run out of memory
+# Make sure we are running the most up2date code
+my.update-all: my.checkout-all
+	# Updating all repos...
+	@for d in $$(ls ..); do                  \
+		if test -d ../$$d/.git; then         \
+			echo -e "\nChecking out $$d";    \
+			git -C ../$$d pull .;            \
+		else                                 \
+			true;                            \
+		fi;                                  \
+	done
+	# Updating all repos...done
+
+# For LabXchange development
+# Ensure only lms and studio are running so we don't run out of memory
 .PHONY: my.labxchange
-my.labxchange: dev.down dev.up.lms dev.up.studio
+my.labxchange:               \
+	requirements             \
+	dev.down                 \
+	dev.pull.lms+studio      \
+	my.update-all            \
+	dev.migrate.lms          \
+	dev.migrate.studio       \
+	dev.up.lms+studio        \
+	dev.check.lms+studio     \
+	dev.check.lms+studio
