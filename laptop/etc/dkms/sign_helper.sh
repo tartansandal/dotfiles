732acdd8cbc02885006f98e19fabf9fc641e681d
Kahlil Hodgson <unknown@email.com>
2022-06-24 17:10:51 +1000

    updates to laptop notes post fresh install

--- laptop/etc/dkms/sign_helper.sh
+++ laptop/etc/dkms/sign_helper.sh
@@ -2,7 +2,11 @@
 
 if [ -d "/run/media/kal/Module Signing" ]
 then
+	echo "Signing module $2"
 	/lib/modules/"$1"/build/scripts/sign-file sha512 \
 		"/run/media/kal/Module Signing/MOK.priv" \
 		"/run/media/kal/Module Signing/MOK.der" "$2"
+else
+	echo "Could not find module signing keys"
+	exit 1
 fi
