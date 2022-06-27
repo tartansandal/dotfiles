84d586c39ca68b42e2e3d05d88aa3fb17987eeb2
Kahlil Hodgson <unknown@email.com>
2022-06-27 11:49:36 +1000

    update notes on MOK and nvidia

--- laptop/etc/dkms/sign_helper.sh
+++ laptop/etc/dkms/sign_helper.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/sh
 
-if [ -d "/run/media/kal/Module Signing" ]
+
+if [ -f "/secret/MOK.priv" ]
 then
 	echo "Signing module $2"
 	/lib/modules/"$1"/build/scripts/sign-file sha512 \
-		"/run/media/kal/Module Signing/MOK.priv" \
-		"/run/media/kal/Module Signing/MOK.der" "$2"
+		/secret/MOK.priv /secret/MOK.der "$2"
 else
 	echo "Could not find module signing keys"
 	exit 1
