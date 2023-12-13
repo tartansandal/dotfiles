698e7f2f89308e121abf6ea94b329aa77d9d4935
Kahlil Hodgson <unknown@email.com>
2023-12-13 16:39:13 +1100

    add plugin for better gx command than netrw

---
+++ nvim/lua/plugins/open-browser.lua
@@ -0,0 +1,8 @@
+return {
+  "tyru/open-browser.vim",
+  keys = {
+    { "gx", "<Plug>(openbrowser-open)", mode = { "n", "v" }, desc = "Open URL under cursor" },
+  },
+}
+
+--  test using https://soylentnews.org/
