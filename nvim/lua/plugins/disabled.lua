9c1d63bd93874e3b902d12496f57a0d7f700175a
Kahlil Hodgson <unknown@email.com>
2024-04-28 10:33:56 +1000

    disable some unused nvim plugins
    reduces number of update notices

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -11,4 +11,14 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     enabled = false,
   },
+
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+  },
+
+  {
+    "catppuccin/nvim",
+    enabled = false,
+  },
 }
