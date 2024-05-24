3a07b4b5de87905e43fc02d26470839c386f74f6
Kahlil Hodgson <unknown@email.com>
2024-05-24 10:06:55 +1000

    chore: move completion config to its own file

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,99 +1,7 @@
 return {
-  -- luasnip
-  -- nvim-cmp
-  -- pairs
   -- surround
   -- comment
   -- ai
-
-  {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
-  },
-
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-emoji",
-      "hrsh7th/cmp-calc",
-      -- "hrsh7th/cmp-spell",
-      -- "hrsh7th/cmp-dictionary",
-    },
-
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      -- load up cmp plugins
-      table.insert(opts.sources, { name = "emoji" })
-      table.insert(opts.sources, { name = "calc" })
-
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0
-          and vim.api
-              .nvim_buf_get_lines(0, line - 1, line, true)[1]
-              :sub(col, col)
-              :match("%s")
-            == nil
-      end
-
-      local luasnip = require("luasnip")
-      local cmp = require("cmp")
-
-      -- opts.window = {
-      --   completion = cmp.config.window.bordered(),
-      --   documentation = cmp.config.window.bordered(),
-      -- }
-
-      -- need force disabling PreselectMode
-      opts.completion.completeopt = "menu,menuone,noinsert,noselect"
-      opts.preselect = cmp.PreselectMode.None
-
-      -- Suppertab-like mapping
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-            cmp.select_next_item()
-          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-          -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<CR>"] = cmp.mapping({
-          -- In INSERT mode only use a completion if explicitly selected
-          i = function(fallback)
-            if cmp.visible() and cmp.get_active_entry() then
-              cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-            else
-              fallback()
-            end
-          end,
-          s = cmp.mapping.confirm({ select = true }),
-          c = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }),
-        }),
-      })
-    end,
-  },
 
   {
     "neovim/nvim-lspconfig",
@@ -117,4 +25,10 @@ return {
   { "tpope/vim-rhubarb", dependencies = { "tpope/vim-fugitive" } },
 
   { "echasnovski/mini.pairs" },
+
+  {
+    -- Need neovim 0.10 for newer versions
+    "lukas-reineke/indent-blankline.nvim",
+    version = "3.5.4",
+  },
 }
