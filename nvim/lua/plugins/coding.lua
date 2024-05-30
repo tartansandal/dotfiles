dcc998add86bbb8696d18155d533e5673f85974a
Kahlil Hodgson <unknown@email.com>
2024-05-30 21:49:31 +1000

    turn on type checking analysis for pyright

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -13,7 +13,8 @@ return {
               analysis = {
                 -- Does not work and may be an anti-pattern
                 -- End up having to set this in config file
-                typeCheckingMode = "off",
+                -- typeCheckingMode = "off",
+                typeCheckingMode = "on",
               },
             },
           },
