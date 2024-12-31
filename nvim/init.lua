60f25aa9f55867162de9e6f8b7a9d1cecda706b5
Kahlil Hodgson <unknown@email.com>
2024-12-31 17:08:32 +1100

    nvim: add snacks.debug functions to global space

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+_G.dd = function(...)
+  Snacks.debug.inspect(...)
+end
+_G.bt = function()
+  Snacks.debug.backtrace()
+end
+vim.print = _G.dd
