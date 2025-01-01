2c29813cc2de0f90df1fe1fce6dc574c50405b9f
Kahlil Hodgson <unknown@email.com>
2025-01-01 21:11:14 +1100

    nvim: use default keymaps for completion

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -37,10 +37,7 @@ return {
         },
       },
       keymap = {
-        preset = "enter",
-        ["<Tab>"] = { "select_next" },
-        ["<S-Tab>"] = { "select_prev" },
-        ["<C-y>"] = { "select_and_accept" },
+        preset = "default",
       },
     },
   },
