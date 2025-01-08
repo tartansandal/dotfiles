ecd125da5902969743122ff5e1514b932de23251
Kahlil Hodgson <unknown@email.com>
2025-01-09 05:48:32 +1100

    nvim: blink - smart capitalisation for buffer word completion

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -34,6 +34,41 @@ return {
             name = "emoji",
             score_offset = 15, -- Tune by preference
           },
+          buffer = {
+            -- keep case of first char
+            transform_items = function(a, items)
+              -- Have to do the following since a.get_keyword() does not work
+              local keyword = a.get_keyword()
+
+              local correct, case
+              if keyword:match("^%l") then
+                correct = "^%u%l+$"
+                case = string.lower
+              elseif keyword:match("^%u") then
+                correct = "^%l+$"
+                case = string.upper
+              else
+                return items
+              end
+
+              -- avoid duplicates from the corrections
+              local seen = {}
+              local out = {}
+              for _, item in ipairs(items) do
+                local raw = item.insertText
+                if raw and raw:match(correct) then
+                  local text = case(raw:sub(1, 1)) .. raw:sub(2)
+                  item.insertText = text
+                  item.label = text
+                end
+                if not seen[item.insertText] then
+                  seen[item.insertText] = true
+                  table.insert(out, item)
+                end
+              end
+              return out
+            end,
+          },
         },
       },
       keymap = {
