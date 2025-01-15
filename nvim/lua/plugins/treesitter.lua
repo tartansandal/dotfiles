a993be2700556c4a3cff455e6c68b05232412768
Kahlil Hodgson <unknown@email.com>
2025-01-15 21:32:30 +1100

    nvim: ensure latex and html treesitters are installed

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     optional = true,
-    opts = { ensure_installed = { "sql" } },
+    opts = { ensure_installed = { "sql", "latex", "html" } },
   },
 }
