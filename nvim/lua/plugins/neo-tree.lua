5ab6d783bc32857b1bfc4e9b35898a2717f9c348
Kahlil Hodgson <unknown@email.com>
2025-02-11 10:07:17 +1100

    nvim: Add config for NeoTree
    - open on right
    - `[tab]` to open but keep focus
    - disable `[b]` so we don't mask buffer delete mapping

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      position = "right",
+      mappings = {
+        ["<tab>"] = function(state)
+          state.commands["open"](state)
+          vim.cmd("Neotree reveal")
+        end,
+        ["b"] = "none",
+      },
+    },
+  },
+}
