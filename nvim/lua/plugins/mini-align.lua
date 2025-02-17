aa5a045fb6056b37a493fce5b5b975b873b0463a
Kahlil Hodgson <unknown@email.com>
2025-02-18 08:07:56 +1100

    add mini-align key mapping

---
+++ nvim/lua/plugins/mini-align.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "echasnovski/mini.align",
+    version = "*",
+    opts = {
+      -- Module mappings. Use `''` (empty string) to disable one.
+      mappings = {
+        start = "ga",
+        start_with_preview = "gA",
+      },
+
+      -- -- Default options controlling alignment process
+      -- options = {
+      --   split_pattern = '',
+      --   justify_side = 'left',
+      --   merge_delimiter = '',
+      -- },
+    },
+  },
+}
