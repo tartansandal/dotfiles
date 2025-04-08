816cc0e3ad1c7f01c5c2c35f36e7c71996be7c8f
Kahlil Hodgson <unknown@email.com>
2025-04-09 08:58:39 +1000

    Increase max number of copilot completions to 3

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -63,7 +63,7 @@ return {
             score_offset = 100,
             async = true,
             opts = {
-              max_completions = 1,
+              max_completions = 3,
               max_attempts = 2,
             },
           },
