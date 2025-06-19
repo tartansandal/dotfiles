6639947e398452a18087ee5a8dbfbf224024354d
Kahlil Hodgson <unknown@email.com>
2025-06-19 22:24:36 +1000

    nvim: disable double esc to normal in lazyvim

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -4,6 +4,10 @@ return {
     opts = {
       lazygit = { enabled = true },
       indent = { enabled = true, scope = { enabled = false } },
+      styles = {
+        -- terminal = { keys = { term_normal = false } },
+        lazygit = { keys = { term_normal = false } },
+      },
     },
   },
 }
