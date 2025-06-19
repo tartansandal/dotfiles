d072530d66303d744b5169fd805b335f793eb931
Kahlil Hodgson <unknown@email.com>
2025-06-19 22:23:42 +1000

    Switch to left side explorer

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -2,7 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     window = {
-      position = "right",
+      -- position = "right",
       mappings = {
         ["<tab>"] = function(state)
           state.commands["open"](state)
