143c2713542319510c0b203409f6581270ab90b4
Kahlil Hodgson <unknown@email.com>
2025-07-31 13:11:15 +1000

    nvim: conform settings to enable injected formating

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -5,6 +5,22 @@ return {
       injected = {
         options = {
           ignore_errors = false,
+          -- Map of treesitter language to file extension
+          -- A temporary file name with this extension will be generated during formatting
+          -- because some formatters care about the filename.
+          lang_to_ext = {
+            bash = "sh",
+            javascript = "js",
+            latex = "tex",
+            markdown = "md",
+            python = "py",
+            typescript = "ts",
+            lua = "lua",
+            sql = "sql",
+          },
+          -- Map of treesitter language to formatters to use
+          -- (defaults to the value from formatters_by_ft)
+          lang_to_formatters = {},
         },
       },
     },
