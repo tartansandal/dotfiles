ffa997dde282942892b7f4c591ae4a1805de86f1
Kahlil Hodgson <unknown@email.com>
2025-08-12 13:47:32 +1000

    nvim: add borders around lsp floating windows

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,6 +2,11 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      diagnostics = {
+        float = {
+          border = "rounded",
+        },
+      },
       servers = {
         pyright = {
           settings = {
@@ -22,6 +27,17 @@ return {
       opts.lsp.signature = {
         auto_open = { enabled = false },
       }
+      opts.presets = {
+        lsp_doc_border = true,
+      }
     end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ui = {
+        border = "rounded",
+      },
+    },
   },
 }
