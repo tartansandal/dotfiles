7b891a0ead6558f840f2ffc180177f7ebceef589
Kahlil Hodgson <unknown@email.com>
2025-08-12 13:46:42 +1000

    nvim: put borders around all floating windows

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,7 +1,14 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local out = vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    lazyrepo,
+    lazypath,
+  })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -31,6 +38,9 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "catppuccin" } },
+  ui = {
+    border = "rounded", -- border style for floating windows
+  },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
