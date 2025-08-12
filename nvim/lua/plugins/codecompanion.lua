1585aece941efb9aff61eac67d2fea5c7fbb751c
Kahlil Hodgson <unknown@email.com>
2025-08-12 22:27:05 +1000

    Update codecompanion config

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -15,6 +15,31 @@ return {
           opts = { add_tool = true, add_slash_command = true, tool_opts = {} },
         },
       },
+      strategies = {
+        -- Change the default chat adapter
+        chat = {
+          adapter = "copilot",
+        },
+      },
+      display = {
+        action_palette = {
+          provider = "default",
+        },
+        chat = {
+          -- show_references = true,
+          -- show_header_separator = false,
+          -- show_settings = false,
+          icons = {
+            tool_success = "󰸞 ",
+          },
+          fold_context = true,
+        },
+        diff = {
+          provider = "mini_diff",
+        },
+      },
+      -- Set debug logging
+      log_level = "DEBUG",
     },
     keys = {
       { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
@@ -27,10 +52,14 @@ return {
       {
         "<leader>ac",
         "<cmd>CodeCompanionChat Toggle<cr>",
-        desc = "Code Companion Actions",
+        desc = "Code Companion Chat",
         mode = { "n", "v" },
       },
     },
+    init = function()
+      -- Create a command line abbreviation (cc) for CodeCompanion
+      vim.cmd([[cab cc CodeCompanion]])
+    end,
   },
 
   {
@@ -38,5 +67,19 @@ return {
     version = "*", -- optional, depending on whether you're on nightly or release
     build = "uv tool upgrade vectorcode", -- optional but recommended if you set `version = "*"`
     dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "codecompanion" },
+  },
+  {
+    "echasnovski/mini.diff",
+    config = function()
+      local diff = require("mini.diff")
+      diff.setup({
+        -- Disabled by default
+        source = diff.gen_source.none(),
+      })
+    end,
   },
 }
