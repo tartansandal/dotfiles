4f33ed388132c77a17e98070086f9eccd06a33a6
Kahlil Hodgson <unknown@email.com>
2025-11-06 14:11:59 +1100

    nvim: disable neotree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,15 +1,18 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    window = {
-      -- position = "right",
-      mappings = {
-        ["<tab>"] = function(state)
-          state.commands["open"](state)
-          vim.cmd("Neotree reveal")
-        end,
-        ["b"] = "none",
-      },
-    },
-  },
+  -- "nvim-neo-tree/neo-tree.nvim",
+  -- dependencies = {
+  --   "antosha417/nvim-lsp-file-operations",
+  -- },
+  -- opts = {
+  --   window = {
+  --     -- position = "right",
+  --     mappings = {
+  --       ["<tab>"] = function(state)
+  --         state.commands["open"](state)
+  --         vim.cmd("Neotree reveal")
+  --       end,
+  --       ["b"] = "none",
+  --     },
+  --   },
+  -- },
 }
