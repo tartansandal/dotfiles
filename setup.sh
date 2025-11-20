0cc67ac7fbc2c4220cdcc91e26965eba80f1c039
Kahlil Hodgson <unknown@email.com>
2025-11-21 10:42:00 +1100

    Remove unused configuration files

--- setup.sh
+++ setup.sh
@@ -11,19 +11,12 @@ mkdir -p ~/tmp/undo ~/tmp/backup
 
 ln -sf "$base_dir"/bash/bashrc ~/.bashrc
 ln -sf "$base_dir"/bash/profile ~/.bash_profile
-ln -sf "$base_dir"/ctags ~/.ctags
-ln -sf "$base_dir"/condarc ~/.condarc
 ln -sf "$base_dir"/dircolors ~/.dircolors
 ln -sf "$base_dir"/direnvrc ~/.direnvrc
 ln -sf "$base_dir"/eslintrc ~/.eslintrc
 ln -sf "$base_dir"/gitconfig ~/.gitconfig
 ln -sf "$base_dir"/gitignore ~/.gitignore
 ln -sf "$base_dir"/inputrc ~/.inputrc
-ln -sf "$base_dir"/jsbeautifyrc ~/.jsbeautifyrc
-ln -sf "$base_dir"/perltidyrc ~/.perltidyrc
-ln -sf "$base_dir"/proverc ~/.proverc
-ln -sf "$base_dir"/remarkrc.yml ~/.remarkrc.yml
-ln -sf "$base_dir"/gitalias ~/.gitalias
 ln -sf "$base_dir"/tmux.conf ~/.tmux.conf
 
 ln -snf "$base_dir"/vim ~/.vim
