d0c9b77faf2053fec64168088f651558a23a116d
Kahlil Hodgson <unknown@email.com>
2026-05-03 10:54:16 +1000

    nvim: add neo-tree close-on-open and auto-preview event handlers

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -2,6 +2,23 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    event_handlers = {
+      {
+        event = "file_open_requested",
+        handler = function()
+          require("neo-tree.command").execute({ action = "close" })
+        end,
+      },
+      {
+        event = "after_render",
+        handler = function(state)
+          if not require("neo-tree.sources.common.preview").is_active() then
+            state.config = { use_float = false }
+            state.commands.toggle_preview(state)
+          end
+        end,
+      },
+    },
     window = {
       -- position = "right",
       mappings = {
