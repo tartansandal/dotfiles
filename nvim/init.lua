f0652ab26d3f4674d3b84a527983b8fa2807d928
Kahlil Hodgson <khodgson@ws-25io.tail0eabd.ts.net>
2026-05-05 15:58:47 +1000

    nvim: disable providers we never use

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-_G.dd = function(...)
-  Snacks.debug.inspect(...)
-end
-_G.bt = function()
-  Snacks.debug.backtrace()
-end
-vim.print = _G.dd
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
