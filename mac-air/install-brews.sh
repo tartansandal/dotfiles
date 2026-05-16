9861f5551e507ad06e2157f3ddc260d56eb43b1b
Kahlil Hodgson <you@example.com>
2026-05-16 15:16:54 +1000

    mac: convert list of brew commands to an installation script

---
+++ mac-air/install-brews.sh
@@ -0,0 +1,28 @@
+#!/bin/env zsh
+
+brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask font-sauce-code-pro-nerd-font
+brew install --cask kitty --appdir=~/Applications
+brew install convert
+brew install coreutils
+brew install direnv
+brew install direnv
+brew install eza
+brew install fd
+brew install fzf
+brew install git
+brew install htop
+brew install imagemagick
+brew install lazygit
+brew install luarocks
+brew install magick
+brew install node
+brew install nvim
+brew install rg
+brew install starship
+brew install syncthing
+brew install tmux
+brew install tree-sitter
+brew install tree-sitter-cli
+brew install utftex
+brew install uv
